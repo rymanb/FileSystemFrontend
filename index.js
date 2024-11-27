217ef0f0540f0c871dcbdb5a51707a2692b72d50
Rymanb <ryman.b@digipen.edu>
2024-11-26 19:10:24 -0800

    Cleaned up code, removed local checks

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const request = require('request');
 const { DefaultAzureCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 
+var config = require('./config.json');
+
 const credential = new DefaultAzureCredential();
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
-
-// if runing locally to test, without internal api's
-var local = true;
-
 
 app.get('/', async (req, res) => {
     // get user email from request stored in X-MS-CLIENT-PRINCIPAL-NAME header
@@ -19,52 +17,42 @@ app.get('/', async (req, res) => {
     
     var userEmail = req.headers['x-ms-client-principal-name'];
     if (!userEmail) {
-        // return all headers if user email is not found
-        if (local) {
-            userEmail = 'boneyshark22@gmail.com';
-        }
-        else {
-            // return not authorized if user email is not found
-            res.status(401).send('Not authorized');
-        }
+        // return not authorized if user email is not found
+        res.status(401).send('Not authorized');
     }
     var userid = userEmail.split('@')[0];
 
     var userJson = { id: userid, userid: userid, email: userEmail };
 
-    var userDBInterfaceUrl = 'https://userdatabaseinterface.internal.wonderfulsky-750ba161.westus2.azurecontainerapps.io/api/user';
+    var userDBInterfaceUrl = config.UserDBUrl + '/api/user';
     var userString = JSON.stringify(userJson);
 
-    const vaultName = "fileservicekeyvault";
-    const url = `https://${vaultName}.vault.azure.net`;
+    const url = config.KeyVaultUrl;
 
     const client = new SecretClient(url, credential);
 
     const authKey = await client.getSecret("UserDBKey");
 
-
-
-
-        // send post request to user database interface to create user
-        // will respont with 200 if it was successful
-        fetch(userDBInterfaceUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-auth-key': authKey.value
-                },
-            body: userString
-            })
-        .then(response => {
-            if (response.status != 200) {
-                throw new Error('User database interface returned status code ' + response.status);
-                }
+    // send post request to user database interface to create user
+    // will respont with 200 if it was successful
+    fetch(userDBInterfaceUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-auth-key': authKey.value
+            },
+        body: userString
+        })
+    .then(response => {
+        if (response.status != 200) {
+            throw new Error('User database interface returned status code ' + response.status);
             }
-            )
-        .catch(error => {
-            console.error('Error:', error);
-            }
-            );
+        }
+        )
+    .catch(error => {
+        console.error('Error:', error);
+        }
+        );
 
 
     // send html, css, and js files
@@ -75,21 +63,13 @@ app.get('/user', async (req, res) => {
     // get user email from request stored in X-MS-CLIENT-PRINCIPAL-NAME header
     var userEmail = req.headers['x-ms-client-principal-name'];
     if (!userEmail) {
-        // return all headers if user email is not found
-        if (local) {
-            userEmail = 'boneyshark22@gmail.com';
-        }
-        else {
-            // return not authorized if user email is not found
-            res.status(401).send('Not authorized');
-        }
+        // return not authorized if user email is not found
+        res.status(401).send('Not authorized');
     }
     
     var userid = userEmail.split('@')[0];
-
-
-    
     res.json({ email: userEmail, userid: userid});
+
     });
 
 app.get('/files', async (req, res) => {
@@ -98,15 +78,13 @@ app.get('/files', async (req, res) => {
         // send file list
         var userEmail = req.headers['x-ms-client-principal-name'];
         if (!userEmail) {
-
             // return not authorized if user email is not found
             throw new Error('User email not found');
-            
         }
 
         var userid = userEmail.split('@')[0];
 
-        var url = 'https://filesystemapp.internal.wonderfulsky-750ba161.westus2.azurecontainerapps.io/listfiles?userid=' + userid;
+        var url = config.FileServiceUrl + '/listfiles?userid=' + userid;
         fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -115,23 +93,8 @@ app.get('/files', async (req, res) => {
     }
     catch (error)
     {
-
-
         console.error('Error:', error);
-        // return test set of files if error occurs
-        if (local)
-        {
-            // wait for 5 seconds before returning test set of files
-            const delay = ms => new Promise(res => setTimeout(res, ms));
-            await delay(1000);
-
-            var data = [{"id":"boneyshark22-M345_Project#2 Rubric.docx","userid":"boneyshark22","filename":"M345_Project#2 Rubric.docx","contenttype":"application/vnd.openxmlformats-officedocument.wordprocessingml.document","contentlength":24657},{"id":"boneyshark22-Upload1txt.txt","userid":"boneyshark22","filename":"Upload1txt.txt","contenttype":"text/plain","contentlength":7},{"id":"boneyshark22-paper.pdf","userid":"boneyshark22","filename":"paper.pdf","contenttype":"application/pdf","contentlength":1915388}];
-            res.json(data);
-        }
-        else
-        {
-            res.status(500).send('Internal server error');
-        }
+        res.status(500).send('Internal server error');
     }
 
     });
@@ -139,20 +102,14 @@ app.get('/files', async (req, res) => {
 app.get('/download', async (req, res) => {
     var userEmail = req.headers['x-ms-client-principal-name'];
     if (!userEmail) {
-        // return all headers if user email is not found
-        if (local) {
-            userEmail = 'boneyshark22@gmail.com';
-        }
-        else {
-            // return not authorized if user email is not found
-            res.status(401).send('Not authorized');
-        }
+        // return not authorized if user email is not found
+        res.status(401).send('Not authorized');
     }
     
 
     var userid = userEmail.split('@')[0];
     var filename = req.query.filename;
-    var url = 'https://filesystemapp.internal.wonderfulsky-750ba161.westus2.azurecontainerapps.io/download?userid=' + userid + '&filename=' + filename;
+    var url = config.FileServiceUrl + '/download?userid=' + userid + '&filename=' + filename;
     // url will return file as attachment, return file as attachment to client
     // cant redirect to url because api is internal and not accessible from client
     const response = await axios({
@@ -173,19 +130,13 @@ app.get('/download', async (req, res) => {
 app.get('/delete', (req, res) => {
     var userEmail = req.headers['x-ms-client-principal-name'];
     if (!userEmail) {
-        // return all headers if user email is not found
-        if (local) {
-            userEmail = 'boneyshark22@gmail.com';
-        }
-        else {
-            // return not authorized if user email is not found
-            res.status(401).send('Not authorized');
-        }
+        // return not authorized if user email is not found
+        res.status(401).send('Not authorized');
     }
 
     var userid = userEmail.split('@')[0];
     var filename = req.query.filename;
-    var url = 'https://filesystemapp.internal.wonderfulsky-750ba161.westus2.azurecontainerapps.io/delete?userid=' + userid + '&filename=' + filename;
+    var url = config.FileServiceUrl + '/delete?userid=' + userid + '&filename=' + filename;
     fetch(url)
     .then(response => {
         if (response.status != 200) {
@@ -207,18 +158,12 @@ app.get('/delete', (req, res) => {
 app.post('/upload', async (req, res) => {
     var userEmail = req.headers['x-ms-client-principal-name'];
     if (!userEmail) {
-        // return all headers if user email is not found
-        if (local) {
-            userEmail = 'boneyshark22@gmail.com';
-        }
-        else {
-            // return not authorized if user email is not found
-            res.status(401).send('Not authorized');
-        }
+        // return not authorized if user email is not found
+        res.status(401).send('Not authorized');
     }
 
     var userid = userEmail.split('@')[0];
-    var url = 'https://filesystemapp.internal.wonderfulsky-750ba161.westus2.azurecontainerapps.io/uploadfile?userid=' + userid;
+    var url = config.FileServiceUrl + '/uploadfile?userid=' + userid;
 
 
     // pipe request to filesystemapp and pipe response to client
@@ -227,10 +172,6 @@ app.post('/upload', async (req, res) => {
 
     }
     );
-
-
-
-
 
 
 app.listen(3000, () => {
